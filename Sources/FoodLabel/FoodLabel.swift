1a6ba912f8028bb31760241712afc07c2e7b7077
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-04 11:11:08 +0500

    tag increment

--- Sources/FoodLabel/FoodLabel.swift
+++ Sources/FoodLabel/FoodLabel.swift
@@ -38,9 +38,16 @@ public struct FoodLabel<DataSource>: View where DataSource: FoodLabelDataSource 
         .padding(15)
         .border(borderColor, width: 5.0)
         .onChange(of: dataSource.energyValue) { newValue in
-            if newValue.unit == .kj && showingEnergyInCalories {
+            
+            let shouldToggle: Bool
+            if newValue.unit == .kj {
+                shouldToggle = showingEnergyInCalories
+            } else {
+                shouldToggle = !showingEnergyInCalories
+            }
+            if shouldToggle {
                 withAnimation {
-                    showingEnergyInCalories = false
+                    showingEnergyInCalories.toggle()
                 }
             }
         }
