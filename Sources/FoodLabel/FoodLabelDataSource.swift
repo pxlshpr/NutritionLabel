f90fbf33ffcde3c485257e7044322d82fe3833ff
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-27 16:44:25 +0500

    tag increment

--- Sources/FoodLabel/FoodLabelDataSource.swift
+++ Sources/FoodLabel/FoodLabelDataSource.swift
@@ -1,50 +1,50 @@
-import Foundation
-import PrepDataTypes
-import FoodLabelScanner
-
-public protocol FoodLabelDataSource: ObservableObject {
-    var energyValue: FoodLabelValue { get }
-    var carbAmount: Double { get }
-    var fatAmount: Double { get }
-    var proteinAmount: Double { get }
-
-    var nutrients: [NutrientType: Double] { get }
-    var showFooterText: Bool { get }
-    var showRDAValues: Bool { get }
-    var allowTapToChangeEnergyUnit: Bool { get }
-    
-    var amountPerString: String { get }
-    var numberOfDecimalPlaces: Int { get }
-}
-
-public extension FoodLabelDataSource {
-    
-    var numberOfDecimalPlaces: Int {
-        1
-    }
-    
-    var allowTapToChangeEnergyUnit: Bool {
-        true
-    }
-    
-    var showFooterText: Bool {
-        true
-    }
-    
-    var showRDAValues: Bool {
-        true
-    }
-    
-    var shouldShowMicronutrients: Bool {
-        !nutrients.filter { !$0.key.isIncludedInMainSection }.isEmpty
-    }
-    
-    var shouldShowCustomMicronutrients: Bool {
-        //TODO: Fix this when custom micros are brought back
-        false
-    }
-    
-    func nutrientAmount(for type: NutrientType) -> Double? {
-        nutrients[type]
-    }
-}
+//import Foundation
+//import PrepDataTypes
+//import FoodLabelScanner
+//
+//public protocol FoodLabelDataSource: ObservableObject {
+//    var energyValue: FoodLabelValue { get }
+//    var carbAmount: Double { get }
+//    var fatAmount: Double { get }
+//    var proteinAmount: Double { get }
+//
+//    var nutrients: [NutrientType: Double] { get }
+//    var showFooterText: Bool { get }
+//    var showRDAValues: Bool { get }
+//    var allowTapToChangeEnergyUnit: Bool { get }
+//    
+//    var amountPerString: String { get }
+//    var numberOfDecimalPlaces: Int { get }
+//}
+//
+//public extension FoodLabelDataSource {
+//    
+//    var numberOfDecimalPlaces: Int {
+//        1
+//    }
+//    
+//    var allowTapToChangeEnergyUnit: Bool {
+//        true
+//    }
+//    
+//    var showFooterText: Bool {
+//        true
+//    }
+//    
+//    var showRDAValues: Bool {
+//        true
+//    }
+//    
+//    var shouldShowMicronutrients: Bool {
+//        !nutrients.filter { !$0.key.isIncludedInMainSection }.isEmpty
+//    }
+//    
+//    var shouldShowCustomMicronutrients: Bool {
+//        //TODO: Fix this when custom micros are brought back
+//        false
+//    }
+//    
+//    func nutrientAmount(for type: NutrientType) -> Double? {
+//        nutrients[type]
+//    }
+//}
