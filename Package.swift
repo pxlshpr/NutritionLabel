3cd95ff3be2a3ca45095dcbd6c3eeb15ca7fc32f
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-06 07:37:10 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.9"),
-        .package(url: "https://github.com/pxlshpr/FoodLabelScanner", from: "0.0.71"),
+        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.108"),
+        .package(url: "https://github.com/pxlshpr/FoodLabelScanner", from: "0.0.81"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
